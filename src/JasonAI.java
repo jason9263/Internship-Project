89380fc5c28b6827ea1e2b82fc29587dd33bb01e
JDIJason <jason9263@163.com>
2016-10-19 11:15:02 -0400

    update

---
+++ src/JasonAI.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2016/9/8.
+ */
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class JasonAI {
+    public static void main(String args[]){
+
+        String str1 = new String("\t");
+        Map<Integer,Integer> jm = new LinkedHashMap<Integer,Integer>();
+
+        jm.put(1,1);
+
+        System.out.print(jm.size());
+
+    }
+}
